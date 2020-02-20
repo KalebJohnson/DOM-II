fbd97ce0af79830ea89a274555b00331871151ec
unknown <KalebJohnson45@yahoo.com>
2020-02-19 19:16:07 -0600

    done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,51 @@
-// Your code goes here
+
+
+//img scale hover
+document.querySelectorAll('.webImg').forEach(item => {
+    item.addEventListener('mouseenter', event => {
+        event.target.style.transform = "scale(1.2)";
+        event.target.style.transition = "all 0.3s";
+    })
+
+    item.addEventListener('mouseleave', event => {
+        event.target.style.transform = "scale(1.0)";
+        event.target.style.transition = "all 0.3s";
+    })
+  })
+
+//nav link color hover
+
+document.querySelectorAll('a').forEach(item => {
+    item.addEventListener('mouseenter', event => {
+        event.target.style.color = 'orange'; 
+    })
+
+    item.addEventListener('mouseleave', event => {
+        event.target.style.color = 'black'; 
+    })
+  })
+
+
+
+//h2 click
+
+document.querySelectorAll('.headerTwo').forEach(item => {
+    item.addEventListener('click', event => {
+        event.target.style.color = 'orange'; 
+    })
+  })
+
+
+
+//btn focus 
+
+document.querySelectorAll('.btn').forEach(item => {
+    item.addEventListener('dblclick', event => {
+        event.target.style.background = 'black'; 
+    })
+  })
+
+
+  
+  
+
